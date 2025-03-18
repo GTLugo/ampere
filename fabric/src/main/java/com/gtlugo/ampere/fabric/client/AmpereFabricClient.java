caed1ddc9cf6166936e919e02061f3a69f11ea0a
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-03-18 00:55:46 -0400

    Formatting

--- fabric/src/main/java/com/gtlugo/ampere/fabric/client/AmpereFabricClient.java
+++ fabric/src/main/java/com/gtlugo/ampere/fabric/client/AmpereFabricClient.java
@@ -3,8 +3,8 @@ package com.gtlugo.ampere.fabric.client;
 import net.fabricmc.api.ClientModInitializer;
 
 public final class AmpereFabricClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
-    }
+  @Override
+  public void onInitializeClient() {
+    // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+  }
 }
